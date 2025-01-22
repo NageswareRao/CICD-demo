a3930a1007e7d9493869540adeb296bb7c56f570
NageswareRao <nageshwar9393@gmail.com>
2025-01-22 14:43:05 +0530

    Update DemocicdApplication.java

--- democicd/src/main/java/com/concentrix/cicd/DemocicdApplication.java
+++ democicd/src/main/java/com/concentrix/cicd/DemocicdApplication.java
@@ -2,12 +2,19 @@ package com.concentrix.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class DemocicdApplication {
+public class DemocicdApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemocicdApplication.class, args);
 	}
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemocicdApplication.class);
+    }
 
 }
