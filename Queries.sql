c0430b1dea942232ca87095125047f4fd59ae875
NageswareRao <nageshwar9393@gmail.com>
2026-06-01 17:57:57 +0530

    Add SQL queries for question responses

--- Queries.sql
+++ Queries.sql
@@ -1,1 +1,11 @@
+SELECT qr.id, qr.option_sequence, qr.response_type_fkid
+FROM msil_supervisor.campaign_question cq
+JOIN msil_supervisor.question_response  qr ON qr.question_fkid = cq.id
+WHERE cq.question_id = :questionId;
 
+SELECT qr.id AS question_response_id, rltm.language_code_fk, rltm.text
+FROM msil_supervisor.campaign_question cq
+JOIN msil_supervisor.question_response          qr   ON qr.question_fkid          = cq.id
+LEFT JOIN msil_supervisor.response_language_text_map rltm ON rltm.question_response_fkid = qr.id
+WHERE cq.question_id = :questionId
+ORDER BY qr.option_sequence, rltm.language_code_fk;
